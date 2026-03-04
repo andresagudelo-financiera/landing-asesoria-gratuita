c6474109128a88cfa25055448dbdfb8e1fc38427
Andres Agudelo <andres@MacBook-Pro-de-Andres-Agudelo.local>
2026-03-04 15:34:12 -0500

    fix: configurar dominio oficial para habilitar miniaturas en redes sociales (OG/SEO)

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://financieramentecompany.com',
   integrations: [react()],
 
   vite: {
